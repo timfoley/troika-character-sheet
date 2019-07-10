2cce09ac0891265c74e012963644e2e3dafff40a
Tim Foley <tim.foley@socialtables.com>
2019-07-09 22:44:52 -0400

    format

--- src/Inventory.js
+++ src/Inventory.js
@@ -4,50 +4,50 @@ import PropTypes from "prop-types";
 import "./Inventory.css";
 
 export function Inventory(props) {
-    const items = [];
+	const items = [];
 
-    for (let i = 0; i < 12; i++) {
-        if (props.inventory[i]) {
-            items.push(
-                <React.Fragment key={"item" + i}>
-                    <li className="item">
-                    <input
-                        id={i}
-                        type="text"
-                        aria-label={`Slot ${i + 1}`}
-                        value={props.inventory[i].name}
-                    />
-                    </li>
-                </React.Fragment>
-            );
-        }
-        else {
-            items.push(
-                <React.Fragment key={"item" + i}>
-                    <li className="item blank">
-                    <input
-                        id={i}
-                        type="text"
-                        aria-label={`Empty slot ${i + 1}`}
-                        value=""
-                    />
-                    </li>
-                </React.Fragment>
-            )
-        }
+	for (let i = 0; i < 12; i++) {
+		if (props.inventory[i]) {
+			items.push(
+				<React.Fragment key={"item" + i}>
+					<li className="item">
+						<input
+							id={i}
+							type="text"
+							aria-label={`Slot ${i + 1}`}
+							value={props.inventory[i].name}
+						/>
+					</li>
+				</React.Fragment>
+			);
+		} else {
+			items.push(
+				<React.Fragment key={"item" + i}>
+					<li className="item blank">
+						<input
+							id={i}
+							type="text"
+							aria-label={`Empty slot ${i + 1}`}
+							value=""
+						/>
+					</li>
+				</React.Fragment>
+			);
+		}
+	}
 
-    }
-
-    return (
-        <div className="inventory">
-        <h2>Inventory</h2>
-        <ol className="inventory__grid">
-            {items}
-        </ol>
-        </div>
-    )
-};
+	return (
+		<div className="inventory">
+			<h2>Inventory</h2>
+			<ol className="inventory__grid">{items}</ol>
+		</div>
+	);
+}
 
 Inventory.propTypes = {
-    inventory: PropTypes.array
-}
+	inventory: PropTypes.array
+};
+
+Inventory.defaultProps = {
+	inventory: [{ name: "", notes: "", quantity: 1, slots: 1 }]
+};
