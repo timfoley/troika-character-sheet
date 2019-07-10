17092358e8db853dab4d062fa87af7ae77ffba56
Tim Foley <tim.foley@socialtables.com>
2019-07-10 13:49:27 -0400

    update prettier rc

--- src/Basics.test.js
+++ src/Basics.test.js
@@ -1,21 +1,21 @@
 import "@testing-library/jest-dom/extend-expect";
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { getQueriesForElement } from "@testing-library/dom";
 
 import char from "./char";
 import { Basics } from "./Basics";
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Basics  character={char} />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
+it("renders without crashing", () => {
+	const div = document.createElement("div");
+	ReactDOM.render(<Basics />, div);
+	ReactDOM.unmountComponentAtNode(div);
+});
 
 it("renders a text input with a label 'Name'", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<Basics  character={char} />, div);
-    const { getByLabelText } = getQueriesForElement(div);
-    const input = getByLabelText(/name/i);
-    expect(input).toHaveAttribute("type", "text");
-})
+	const div = document.createElement("div");
+	ReactDOM.render(<Basics character={char} />, div);
+	const { getByLabelText } = getQueriesForElement(div);
+	const input = getByLabelText(/name/i);
+	expect(input).toHaveAttribute("type", "text");
+});
